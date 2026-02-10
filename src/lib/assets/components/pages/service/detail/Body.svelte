39811ce50937b8344fff571909aa98cd02bb856d
Dmytro Gul <guldmytro@gmail.com>
2026-02-10 10:36:41 +0200

    added reactivity

--- src/lib/assets/components/pages/service/detail/Body.svelte
+++ src/lib/assets/components/pages/service/detail/Body.svelte
@@ -13,8 +13,6 @@
     import { replaceStringWithCity } from "$lib/utils";
 
     import { page } from "$app/state";
-
-    let city = $derived(page.data?.currentCity);
 </script>
 
 {#if body && textSide && listSide}
@@ -22,25 +20,25 @@
         <Container>
             <div class="body-row">
                 <div class="text-side">
-                    <Headline title={replaceStringWithCity(textSide.title, city)} level={2} levelStyle={2} />
+                    <Headline title={replaceStringWithCity(textSide.title, page.data?.currentCity || '')} level={2} levelStyle={2} />
                     {#if Array.isArray(textSide.paragraphs) && textSide.paragraphs.length}
                         <div class="text-side__content">
                             {#each textSide.paragraphs as text}
-                                <Paragraph text={replaceStringWithCity(text, city)} size="p" weight="400" />
+                                <Paragraph text={replaceStringWithCity(text, page.data?.currentCity || '')} size="p" weight="400" />
                             {/each}
                         </div>
                     {/if}
                 </div>
                 {#if Array.isArray(listSide.items) && listSide.items.length}
                     <div class="list-side">
-                        <Headline title={replaceStringWithCity(listSide.title, city)} level={3} levelStyle={4} />
+                        <Headline title={replaceStringWithCity(listSide.title, page.data?.currentCity || '')} level={3} levelStyle={4} />
                         <ul class="list">
                             {#each listSide.items as text}
                                 <li class="list-item">
                                     <span class="list-item__checked">
                                         <Sprite id="checked" />
                                     </span>
-                                    <Paragraph text={replaceStringWithCity(text, city)} size="p" weight="400" />
+                                    <Paragraph text={replaceStringWithCity(text, page.data?.currentCity || '')} size="p" weight="400" />
                                 </li>
                             {/each}
                         </ul>
