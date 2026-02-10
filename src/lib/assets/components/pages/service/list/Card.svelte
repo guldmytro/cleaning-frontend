dfced7ec61ab4d3c6e4cf4fc86db1d6c10b69e9a
Dmytro Gul <guldmytro@gmail.com>
2026-02-10 14:31:23 +0200

    Added hover effect

--- src/lib/assets/components/pages/service/list/Card.svelte
+++ src/lib/assets/components/pages/service/list/Card.svelte
@@ -9,8 +9,8 @@
     export let category: CategoryDetail;
 </script>
 
-<li class="service" 
-    style="background: linear-gradient(180deg, rgba(21, 25, 31, 0.10) 0%, rgba(21, 25, 31, 0.80) 100%), url({service.image}) no-repeat center / cover">
+<li class="service">
+    <img src={service.image} alt={service.short_title} loading="lazy">
     <div class="service-content gr">
         <Headline title={service.short_title} level={2} levelStyle={4} href={localizeHref(`/${page?.data?.currentCitySlug}/services/${category.slug}/${service.slug}`)} />
         <LinkArrow href={localizeHref(`/${page?.data?.currentCitySlug}/services/${category.slug}/${service.slug}`)} ariaLabel={service.short_title} />
@@ -19,6 +19,7 @@
 
 <style>
     .service {
+        position: relative;
         display: grid;
         align-items: end;
         width: 100%;
@@ -27,10 +28,50 @@
         border-radius: 10px;
         padding: var(--s-h-400);
         color: var(--c-white);
+        overflow: hidden;
+        isolation: isolate;
+    }
+
+    .service::after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(180deg, rgba(21, 25, 31, 0.10) 0%, rgba(21, 25, 31, 0.80) 100%);
+        z-index: 1;
+    }
+
+    .service img {
+        position: absolute;
+        z-index: -1;
+        transition: transform 800ms ease;
+    }
+
+    .service:hover img {
+        transform: scale(1.1);
     }
 
     .service-content {
         row-gap: var(--s-v-300);
+        position: relative;
+        z-index: 2;
+    }
+
+    :global(.service .headline__link) {
+        position: relative;
+    }
+
+    :global(.service .headline__link::before) {
+        content: '';
+        display: block;
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+        right: -9999px;
+        bottom: -9999px;
     }
 
     @media (max-width: 834px) {
