0ca2302a8406f36fc48785524cec79e5ec3acec4
Dmytro Gul <guldmytro@gmail.com>
2026-02-18 18:30:12 +0200

    Fixed

--- src/lib/assets/components/sections/FormStages.svelte
+++ src/lib/assets/components/sections/FormStages.svelte
@@ -537,5 +537,10 @@
             display: grid;
             grid-template-columns: repeat(2, minmax(0, 1fr));
         }
+
+        :global(.bottom-controls button) {
+            width: 100%;
+            text-align: center;
+        }
     }
 </style>
