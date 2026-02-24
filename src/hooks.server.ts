cf02838a7168a38bd732a993c1421ae309d56d3e
Dmytro Gul <guldmytro@gmail.com>
2026-02-24 15:26:46 +0200

    Fixed

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,23 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import { redirect } from '@sveltejs/kit';
+
+const redirects: Record<string, string> = {
+	'/impressum': '/zuerich/legal-notice',
+	'/kontakt': '/zuerich/contacts',
+	'/reinigungsfirma-zuerich': '/zuerich',
+	'/umzugsreinigung-zuerich': '/zuerich/services/spezialreinigung/umzugsreinigung',
+	'/reinigungsfirma-berlin-gebaeudereinigung-experten-bern': '/bern'
+};
 
 const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
 	event.request = request;
+
+	const pathname = event.url.pathname;
+
+	if (redirects[pathname]) {
+		throw redirect(301, redirects[pathname]);
+	}
 
 	return resolve(event, {
 		transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
