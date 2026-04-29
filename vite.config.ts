3840c05e7dfef2dd433f11f7af229d24caa1d7ff
Dmytro Gul <guldmytro@gmail.com>
2026-04-29 15:12:44 +0300

    Fixed redirect

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 		paraglideVitePlugin({ 
 			project: './project.inlang', 
 			outdir: './src/lib/paraglide',
-			strategy: ['cookie', 'preferredLanguage', 'url'],
+			strategy: ['cookie', 'url'],
 			urlPatterns: [
 				{
 					pattern: "/:path(.*)?",
