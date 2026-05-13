88274ce833b45d72ee3a8069a1098b351dacc4f0
Dmytro Gul <guldmytro@gmail.com>
2026-05-13 18:35:54 +0300

    Removed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,15 +39,6 @@
 			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 		})(window, document, "clarity", "script", "vr04b2sm8j");
 	</script>
-	
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-K7SZRW7T');</script>
-	<!-- End Google Tag Manager -->
-
 
 	<!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -57,10 +48,6 @@
 	})(window,document,'script','dataLayer','GTM-P2KVT7M3');</script>
 	<!-- End Google Tag Manager -->
 </svelte:head>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7SZRW7T"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2KVT7M3"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
